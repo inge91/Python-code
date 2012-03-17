25354eb73ef582461fa4fee88c1bf033074cbb6b
Inge <inge.becht91@gmail.com>
2012-03-17 14:36:11 +0100

    woking p vs. p

--- tic-tac-toe/Board.py
+++ tic-tac-toe/Board.py
@@ -8,10 +8,7 @@ import Players as p
 
 
 class board:
-    board = []
-    turn = None
-    lastTurn = None
-   # def __init__(self):
+    # def __init__(self):
    #     # the board
     #    self.board =[p.NONE, p.NONE, p.NONE,
      #           p.NONE, p.NONE, p.NONE,
@@ -28,139 +25,147 @@ class board:
 
     # determine if someone has won
     def winner(self):
-        if (board.checkHorizontal(self) is not p.NONE):
+        if (board.checkHorizontal(self) is not p.Players.NONE):
+            print "horizontal win"
             return board.checkHorizontal(self)
-        elif (board.checkVertical(self) is not p.NONE):
+        elif (board.checkVertical(self) is not p.Players.NONE):
+            print "vertical win"
             return board.checkVertical(self)
-        elif (board.CheckDiagonal(self) is not p.NONE):
+        elif (board.checkDiagonal(self) is not p.Players.NONE):
+            print "diagonal win"
             return board.checkDiagonal(self)
         else: 
-            return p.NONE
-    
+            return p.Players.NONE
+
     # check if there is a win in the horizontal row of the last turn
     def checkHorizontal(self):
-        global lastTurn
-        global board
-        tempTurn = lastTurn
-        if (lastTurn == 1 or lastTurn == 2):
+        tempTurn = self.lastTurn
+        if (self.lastTurn == 1 or self.lastTurn == 2):
             tempTurn = 0
-        elif (lastTurn == 4 or lastTurn == 5):
+        elif (self.lastTurn == 4 or self.lastTurn == 5):
             tempTurn = 3
         else:
             tempTurn = 6
-        
-        if ( board[tempTurn] == p.CROSS and board[tempTurn + 1] == p.CROSS and
-                board[tempTurn + 1] == p.CROSS):
-            return p.CROSS
-        elif ( board[tempTurn] == p.CIRKLE and board[tempTurn + 1] == p.CIRKLE and
-                board[tempTurn + 1] == p.CIRKLE):
-            return p.CIRKLE
-        else: return p.NONE
-    
+
+        if ( self.board[tempTurn] == p.Players.CROSS 
+                and self.board[tempTurn +
+                    1] == p.Players.CROSS and
+                self.board[tempTurn + 2] == p.Players.CROSS):
+            return p.Players.CROSS
+        elif ( self.board[tempTurn] == p.Players.CIRKLE
+                and self.board[tempTurn + 1] == p.Players.CIRKLE and
+                self.board[tempTurn + 2] == p.Players.CIRKLE):
+            return p.Players.CIRKLE
+        else: return p.Players.NONE
+
     def checkVertical(self):
-        global lastTurn
-        global board
-        tempTurn = lastTurn
-        if (lastTurn == 3 or lastTurn == 6):
+        tempTurn = self.lastTurn
+        print tempTurn
+        if (self.lastTurn == 3 or self.lastTurn == 6):
             tempTurn = 0
-        elif (lastTurn == 4 or lastTurn == 7):
+        elif (self.lastTurn == 4 or self.lastTurn == 7):
             tempTurn = 1
         else:
             tempTurn = 2
 
-        if ( board[tempTurn] == p.CROSS and board[tempTurn + 3] == p.CROSS and
-                board[tempTurn + 6] == p.CROSS):
-            return p.CROSS
-        elif ( board[tempTurn] == p.CIRKLE and board[tempTurn + 2] == p.CIRKLE and
-                board[tempTurn + 6] == p.CIRKLE):
-            return p.CIRKLE
+        if ( self.board[tempTurn] == p.Players.CROSS and
+                self.board[tempTurn + 3] == p.Players.CROSS and
+                self.board[tempTurn + 6] == p.Players.CROSS):
+            return p.Players.CROSS
+        elif ( self.board[tempTurn] == p.Players.CIRKLE and 
+                self.board[tempTurn + 3] == p.Players.CIRKLE and
+                self.board[tempTurn + 6] == p.Players.CIRKLE):
+            return p.Players.CIRKLE
         else:
-            return p.NONE
+            return p.Players.NONE
 
 
     def checkDiagonal(self):
-        global lastTurn
-        global board
-        listdiag = [[board[0], board[4], board[7]], [board[6], board[4],
-            board[2]]]
+        listdiag = [[self.board[0], self.board[4], self.board[7]], [self.board[6], self.board[4],
+            self.board[2]]]
         listdiagone = listdiag[0]
         listdiagtwo = listdiag[1]
-        
-        if ( ((p.CROSS in listdiagone) and (p.CIRKLE not in listdiagone)) or
-                ((p.CROSS in listdiagtwo) and (p.CIRKLE not in
-                    listdiagtwo))):
-            return p.CROSS
-        elif (((p.CROSS not in listdiagone) and( p.CIRKLE in
-            listdiagone)) or
-                ((p.CROSS not in listdiagtwo) and (p.CIRKLE in
-                    listdiagtwo))):
-            return p.CIRKLE
+
+        if ( ((p.Players.CROSS in listdiagone) and (p.Players.CIRKLE not in
+            listdiagone and p.Players.NONE not in listdiagone)) or
+                ((p.Players.CROSS in listdiagtwo) and (p.Players.CIRKLE not in
+                    listdiagtwo and p.Players.NONE not in listdiagtwo))):
+                    return p.Players.CROSS
+        elif (((p.Players.CROSS not in listdiagone) and( p.Players.CIRKLE in
+            listdiagone and p.Players.NONE not in listdiagone)) or
+            ((p.Players.CROSS not in listdiagtwo) and (p.Players.CIRKLE in
+                listdiagtwo and p.Players.NONE not in listdiagtwo))):
+                return p.Players.CIRKLE
         else:
-            return p.NONE
+            return p.Players.NONE
 
-        # checks if slot is full
-        def isfull(self, slot):
-            global board
-            if (board[slot] == p.NONE):
-                return False
+    # checks if slot is full
+    def isfull(self, slot):
+        if (self.board[slot] == p.Players.NONE):
+            return False
+        else:
+            return True
+
+    # make a move and change turn and lastTurn
+    def makeMove(self, slot):
+        if (board.isFull(self, slot) or slot < 0 or slot > 8) :
+            print "Invalid move"
+            return False
+        else:
+
+            self.board[slot] = self.turn
+            if (self.turn == p.Players.CROSS):
+                self.turn = p.Players.CIRKLE
             else:
-                return True
-        
-        # make a move and change turn and lastTurn
-        def makeMove(self, slot):
-            global turn
-            global lastTurn
-            if (isFull(slot) or slot < 0 or slot > 8) :
-                print "Invalid move"
-                return false
-            else:
-                if (turn == p.CROSS):
-                    turn = p.CIRKLE
-                else:
-                    turn = p.CIRKLE
-                lastTurn = slot
-                return true
-        
+                self.turn = p.Players.CROSS
+            self.lastTurn = slot
+            return True
+
+    # checks if slot is full
+    def isFull(self, slot):
+        if(self.board[slot] == p.Players.NONE):
+            return False
+        else:
+            return True
+
         # return a list with all possible children of the board
-        def children(self):
-            global board
-            global turn
-            all_children
-            copy = []
-            for i in range(0, len(board)) :
-                for j in range(0, len(board)) :
-                    # make a new empty gameboard
-                    copy = copy + [board[j]]
-                if (turn == p.CROSS) :
-                    child = Board(copy, lastTurn, turn)
-                if (turn == p.CIRKLE) :
-                    child = Board(copy, lastTurn, turn)
-                # in case the move is possible add to all_children
-                if( child.move(i) == true ) : 
-                    all_children = all_children + child
-            return all_children
+    def children(self):
+        all_children
+        copy = []
+        for i in range(0, len(self.board)) :
+            for j in range(0, len(self.board)) :
+                # make a new empty gameboard
+                copy = copy + [self.board[j]]
+            if (turn == p.Players.CROSS) :
+                child = Board(copy, self.lastTurn, turn)
+            if (turn == p.Players.CIRKLE) :
+                child = Board(copy, self.lastTurn, turn)
+            # in case the move is possible add to all_children
+            if( child.move(i) == true ) : 
+                all_children = all_children + child
+        return all_children
 
-    
-        def printBoard(self):
-            global board
-            print "Board state:"
-            a = ''
-            for i in range (0, len(board)):
-                if (board[i] == p.CROSS):
-                    a = 'x'
-                elif (board[i] == p.CIRKLE):
-                    a = 'o'
-                else:
-                    a = '_'
-                if ( i == 2 or i == 5):
-                    print i
-                else: print i,
-        
-            if (board[i] == p.CROSS):
+
+    def printBoard(self):
+        print "Board state:"
+        a = ''
+        for i in range (0, len(self.board)):
+            if (self.board[i] == p.Players.CROSS):
                 a = 'x'
-            elif (board[i] == p.CIRKLE):
+            elif (self.board[i] == p.Players.CIRKLE):
                 a = 'o'
             else:
                 a = '_'
-            print "you are: " + a
-                
+            if ( i == 2 or i == 5):
+                print a
+            else: print a,
+        
+        if (self.turn == p.Players.CROSS):
+            a = 'x'
+        elif (self.turn == p.Players.CIRKLE):
+            a = 'o'
+        else:
+            a = '_'
+        print
+        print "you are: " + a
+
--- tic-tac-toe/TextInterface.py
+++ tic-tac-toe/TextInterface.py
@@ -1,7 +1,7 @@
 
 import Players as p
 import Board 
-game = Board.board([0,0,0,0,0,0,0,0,0],None,None)
+game = Board.board([0,0,0,0,0,0,0,0,0],None,p.Players.CROSS)
 class TextInterface:
 
     def __init__(self):
@@ -11,22 +11,42 @@ class TextInterface:
         global Board
         global game
         print "How would you like to play?"
-        print "press 1 for p vs p"
+        print "press 1 for p vs p \n >>"
         answer = raw_input()
         if(answer == '1'):
+            print "answer was 1"
             while(True):
                 TextInterface.playerMove(self)
-                if (game.winner() is not p.NONE):
+                game.printBoard()
+                if (game.winner() is not p.Players.NONE):
                     break
-                printBoard()
-        print "its a draw or someone won"
+            if (game.winner() == p.Players.CROSS):
+                print "cross won!"
+            elif (game.winner() == p.Players.CIRKLE):
+                print "cirkle won!"
+            else:
+                print "draw!"
+            print "want to play again?[y/n] \n >>"
+            answer = raw_input()
+            while(answer is not 'y' or answer is not 'n'):
+                print "illegal input!"
+                print "want to play again?[y/n] \n >>"
+                answer = raw_input()
+            if(answer == 'y'):
+                TextInterface.gameLoop(self)
+            else:
+                System.exit(0)
+        else:
+            print "Illegal input"
+            TextInterface.gameloop(self)
 
 
-    def playerMove(game):
-        moveValid = True
-        while(not moveValid):
-            print "Make a move!"
+    def playerMove(self):
+        moveValid = False
+        while(moveValid == False):
+            print "Make a move! \n >>"
             i = raw_input()
-            moveValid = game.move(i)
+            moveValid = game.makeMove(int(i))
+            print moveValid
 
 
